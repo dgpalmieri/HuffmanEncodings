cb93ab8f286ad6c9bd24219901bb9c767e69a529
Dylan Palmieri <dgpalmieri@gmail.com>
2020-12-02 02:12:37 -0900

    Implement encoding and decoding

--- huffcode.cpp
+++ huffcode.cpp
@@ -13,6 +13,7 @@
 #include "huffcode.hpp"  // for class HuffCode declaration
 #include <string>
 using std::string;
+using std::to_string;
 #include <unordered_map>
 using std::unordered_map;
 #include<memory>
@@ -21,7 +22,11 @@ using std::make_shared;
 #include<utility>
 using std::pair;
 using std::make_pair;
+#include<algorithm>
 
+#include<iostream> // TODO remove these
+using std::cout;
+using std::endl;
 
 // Node Class
 // For generating Huffman Codes
@@ -40,48 +45,72 @@ class Node {
         explicit Node( const ValType & data, const int & weight,
                                              shared_ptr< Node > lc = nullptr,
                                              shared_ptr< Node > rc = nullptr )
-            : _data(data), _leftChild(lc), _rightChild(rc) {}
+            : _data(data), _leftChild(lc), _rightChild(rc) { }
 
         ~Node() = default;
 };
 
+// TODO remove this
+void printBT(const std::string& prefix, const shared_ptr<Node< char >> & node, bool isLeft)
+{
+    if( node != nullptr )
+    {
+        std::cout << prefix;
 
-void generateTree( Node< char > & head,
+        std::cout << (isLeft ? "├──" : "└──" );
+
+        // print the value of the node
+        std::cout << node->_data << std::endl;
+
+        // enter the next tree level - left and right branch
+        printBT( prefix + (isLeft ? "│   " : "    "), node->_leftChild, true);
+        printBT( prefix + (isLeft ? "│   " : "    "), node->_rightChild, false);
+    }
+}
+
+// TODO remove this
+void printBT(const shared_ptr<Node<char>> node)
+{
+    printBT("", node, false);
+}
+
+
+void generateTree( shared_ptr< Node< char > > & head,
                    std::priority_queue< std::pair<int, char>,
                        std::vector< std::pair< int, char > >,
-                       std::greater< std::pair< int, char > > > pq ){
+                       std::greater< std::pair< int, char > > > pq ) {
 
     while( !pq.empty() ){
         auto top = pq.top();
 
-        if( head._leftChild == nullptr ){
-            head._leftChild = make_shared< Node< char > >
+        if( head->_leftChild == nullptr ){
+            head->_leftChild = make_shared< Node< char > >
                               ( Node< char >(top.second, top.first) );
             pq.pop();
             continue;
         }
 
-        if ( head._rightChild == nullptr ){
-            head._rightChild = make_shared< Node< char > >
+        if ( head->_rightChild == nullptr ){
+            head->_rightChild = make_shared< Node< char > >
                                ( Node< char >(top.second, top.first) );
             pq.pop();
             continue;
         }
 
-        /*
-         *auto nextChild = make_shared< Node< char > >( Node< char >(top.second,
-         *                                                           top.first) );
-         */
-        head._leftChild = make_shared< Node< char > >( Node< char >
-                                     ( 0, 0, head._leftChild ) );
+        int leftWeight = head->_leftChild ?
+                         head->_leftChild->_weight : 0;
 
-        pq.pop();
+        int rightWeight = head->_rightChild ?
+                          head->_rightChild->_weight : 0;
+
+        head = make_shared< Node< char > >( Node< char >
+                        ( 0, leftWeight + rightWeight, head ) );
+
     }
 }
 
 
-void HuffCode::setWeights( const unordered_map< char, int > & theweights )
-{
+void HuffCode::setWeights( const unordered_map< char, int > & theweights ) {
     if( theweights.empty() )
         return;
 
@@ -90,16 +119,41 @@ void HuffCode::setWeights( const unordered_map< char, int > & theweights )
 }
 
 
-string HuffCode::encode( const string & text ) const
-{
+string HuffCode::encode( const string & text ) const {
     if( text.empty() )
         return "";
 
-    Node< char > head( 0, 0 );
+    auto head = make_shared< Node< char > >( Node< char >( 0, 0 ) );
 
     generateTree( head, _pqueue );
 
-    return "";  // DUMMY RETURN
+    string total = "";
+    for ( const auto & character : text ) {
+        auto tempHead(head);
+
+        while( true ){
+            if ( tempHead->_rightChild &&
+                 tempHead->_rightChild->_data == character ) {
+                total += "1";
+                break;
+            }
+            else if ( tempHead->_leftChild &&
+                      tempHead->_leftChild->_data == character ) {
+                total += "0";
+                break;
+            }
+            else {
+                total += "0";
+                if ( tempHead->_leftChild ){
+                    tempHead = tempHead->_leftChild ;
+                }
+                else {
+                    break; }
+            }
+        }
+    }
+
+    return total;
 }
 
 
@@ -108,10 +162,28 @@ string HuffCode::decode( const string & codestr ) const
     if( codestr.empty() )
         return "";
 
-    Node< char > head( 0, 0 );
+    auto head = make_shared< Node< char > >( Node< char >( 0, 0 ) );
 
     generateTree( head, _pqueue );
 
-    return "";  // DUMMY RETURN
+    string ret;
+    auto tempHead = head;
+    for ( const char & character : codestr ){
+        if ( character == '0' ) {
+            if ( tempHead->_leftChild != nullptr ){
+                tempHead = tempHead->_leftChild;
+                if ( tempHead->_leftChild == nullptr ) {
+                    ret += tempHead->_data;
+                    tempHead = head;
+                }
+            }
+        }
+        else if ( character == '1' ) {
+            ret += tempHead->_rightChild->_data;
+            tempHead = head;
+        }
+    }
+
+    return ret;  // DUMMY RETURN
 }
 
